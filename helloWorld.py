ff5c50cddd3e1882846fcfe4f6c3653a14b472a8
Miguel R. Araújo <143842829+Code016m@users.noreply.github.com>
2026-05-11 12:08:53 -0300

    Add salutation route for GET and POST requests

--- helloWorld.py
+++ helloWorld.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def hello_world():
     return jsonify({'message': 'Hello World!'})
 
+# Rota POST
+@app.route('/salutation', methods=['GET', 'POST'])
+def salutation():
+    if request.method == 'POST':
+        data = request.get_json()
+        name = data.get('name')
+    else:
+        name = request.args.get('name', 'Guest')
+    return jsonify({
+        'message': 'Hello %s!' % name
+    })
+
 if __name__ == "__main__":
     app.run(debug=True)
